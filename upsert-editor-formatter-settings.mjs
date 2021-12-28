558ccdede1536b14951b6ca883b20db7ea2b057d
git-masi <eric.masi.dev@gmail.com>
2021-12-28 17:53:05 -0500

    Edit existing settings file

--- upsert-editor-formatter-settings.mjs
+++ upsert-editor-formatter-settings.mjs
@@ -1,8 +1,11 @@
-import { mkdir, stat, writeFile } from 'fs/promises';
+import { execSync } from 'child_process';
+import { mkdir, readFile, stat, writeFile } from 'fs/promises';
 import { cwd } from 'process';
 
 (async () => {
-  const projectDir = cwd();
+  const projectDirPath = cwd();
+  const vscodeDirPath = `${projectDirPath}/.vscode`;
+  const settingsFilePath = `${vscodeDirPath}/settings.json`;
   const editorSettings = {
     'editor.defaultFormatter': 'esbenp.prettier-vscode',
     '[javascript]': {
@@ -27,11 +30,23 @@ import { cwd } from 'process';
 
   if (!hasVscodeDir) await createVscodeDir();
 
-  if (!hasSettingsFile) await createSettingsFile();
+  // return early if we create the file with the data
+  if (!hasSettingsFile)
+    return await writeSettingsFile(JSON.stringify(editorSettings));
+
+  const currentSettings = JSON.parse(await readSettingsfile());
+
+  Object.keys(editorSettings).forEach(
+    (key) => (currentSettings[key] = editorSettings[key])
+  );
+
+  await writeSettingsFile(JSON.stringify(currentSettings));
+
+  execSync('npx prettier --write "./.vscode/settings.json"');
 
   async function vscodeDirExists() {
     try {
-      const dir = await stat(`${projectDir}/.vscode`);
+      const dir = await stat(vscodeDirPath);
       return dir.isDirectory();
     } catch (error) {
       return false;
@@ -40,7 +55,7 @@ import { cwd } from 'process';
 
   async function settingsFileExists() {
     try {
-      const file = await stat(`${projectDir}/.vscode/settings.json`);
+      const file = await stat(settingsFilePath);
       return file.isFile();
     } catch (error) {
       return false;
@@ -48,13 +63,14 @@ import { cwd } from 'process';
   }
 
   async function createVscodeDir() {
-    await mkdir(`${projectDir}/.vscode`);
+    await mkdir(vscodeDirPath);
   }
 
-  async function createSettingsFile() {
-    await writeFile(
-      `${projectDir}/.vscode/settings.json`,
-      JSON.stringify(editorSettings)
-    );
+  async function writeSettingsFile(data) {
+    await writeFile(settingsFilePath, data);
+  }
+
+  async function readSettingsfile() {
+    return readFile(settingsFilePath, { encoding: 'utf8' });
   }
 })();

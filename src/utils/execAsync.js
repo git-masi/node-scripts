549506e0e13637ee69167ef05536b1c8102cdf10
git-masi <eric.masi.dev@gmail.com>
2021-12-29 09:02:27 -0500

    Add jsdoc

--- src/utils/execAsync.js
+++ src/utils/execAsync.js
@@ -2,6 +2,6 @@ import { exec } from 'child_process';
 import util from 'util';
 
 /**
- * returns Promise<{stdout, stderr}>
+ * @returns {Promise<{stdout, stderr}>}
  */
 export const execAsync = util.promisify(exec);
